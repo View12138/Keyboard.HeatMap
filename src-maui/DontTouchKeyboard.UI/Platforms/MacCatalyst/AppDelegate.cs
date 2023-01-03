a991c8d43196bb4e05c802bb5724cb42951d590d
View <ms_view@outlook.com>
2023-01-03 09:18:25 +0800

    dev: remove maui

--- src-maui/DontTouchKeyboard.UI/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace DontTouchKeyboard.UI;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
